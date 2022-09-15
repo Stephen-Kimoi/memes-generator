96e82aa68721dee0f9d5b26cb222532e513656ec
Steve Kimoi <moenisteve@gmail.com>
2022-09-15 12:03:04 +0300

    adding the form input functionality

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -2,20 +2,42 @@ import React,{useState} from 'react'
 import memesData from '../../Data/memesData'
 
 const Meme = () => { 
+
   const [meme, setMeme] = useState({
     topText: "", 
     bottomText: "", 
     randomImage: "http://i.imgflip.com/1bij.jpg", 
   }); 
-  const [allMemeImages, setAllMemeImages] = useState([memesData])
 
-  const handleButton = () => {
+  const [allMemeImages, setAllMemeImages] = useState([memesData]); 
+
+  // const [formInput, setFormInput] = useState({
+  //   topText: "", 
+  //   bottomText: ""
+  // })
+
+  // console.log(formInput)
+
+  const handleChange = (event) => {
+      const {name, value} = event.target
+
+      setMeme( prevMeme => {
+        return {
+          ...prevMeme, 
+          [name]: value
+        }
+      })
+  }
+
+  const handleButton = (event) => {
     const memesArray = allMemeImages[0].data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length) 
     const url = memesArray[randomNumber].url
+    const {name, value} = event.target
 
     setMeme( prevMeme => ({
         ...prevMeme, 
+        [name]: value, 
         randomImage: url
     }))
 
@@ -29,11 +51,17 @@ const Meme = () => {
                 className='input'
                 type='text' 
                 placeholder='Top text'
+                name="topText"
+                value={meme.topText}
+                onChange={handleChange}
             /> 
             <input 
                 className='input'
                 type='text'
                 placeholder='Bottom text'
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}
             />
            <button className="submit-button" type='submit' onClick={handleButton}>
               Get a new meme image 🖼
@@ -42,6 +70,8 @@ const Meme = () => {
 
        <div className='meme-div'>
            <img src={meme.randomImage} alt="meme" className='meme-url'/>
+           <h2 className='meme-text top'>{meme.topText}</h2> 
+           <h2 className='meme-text bottom'>{meme.bottomText}</h2>
        </div>
 
 
