e115ecc5522e875b24d297d653178b6dd6350f37
Steve Kimoi <moenisteve@gmail.com>
2022-09-16 17:11:57 +0300

    adding the footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <a href='https://kimoisteve.me' target="_blank">&copy; Steve Kimoi</a>
+    </footer>
+  )
+}
+
+export default Footer
