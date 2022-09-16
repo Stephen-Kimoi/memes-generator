191971adc3c7d4a486ba74f07c6caeb34d8e05f8
Steve Kimoi <moenisteve@gmail.com>
2022-09-16 17:13:19 +0300

    adding navigation and paths

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import troll_face from '../assets/troll-face.png'; 
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  let navigate = useNavigate(); 
   return (
     <header>
             <img src={troll_face} alt="troll face" className='header-img'/>
             <h1 className='header-title'>Meme Generator</h1>
-            <p className='header-description'>React Project 3</p>
+            <p 
+              className='header-description' 
+              href="/instructions" 
+              onClick={ () => {
+                navigate("/instructions")
+              }}
+            > Click for<br/>Insructions</p>
     </header>
   )
 }
