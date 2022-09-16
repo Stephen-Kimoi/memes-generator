615c92e2f38ec5823cfbb287a7e9f4fb606fed3c
Steve Kimoi <moenisteve@gmail.com>
2022-09-16 17:15:20 +0300

    adding browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Meme from './components/Meme/Meme'
+import Instructions from './components/Instructions/Instructions'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header /> 
-      <Meme />  
-    </>
+      <Routes>
+         <Route path={"/"} element={<Meme />}/> 
+         <Route path={"/instructions"} element={<Instructions/>}/> 
+      </Routes>
+      {/* <Footer/> */}
+    </BrowserRouter>
   )
 }
 
